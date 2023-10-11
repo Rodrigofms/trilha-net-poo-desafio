cf3cc411e78fe2f3b3c66b5f2d3b757ea202e14d
Rodrigofms <rodrigoms.rec@gmail.com>
2023-10-11 16:52:34 -0300

    Finalização do projeto

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} da Play Store em seu Nokia...");
+        }
+        public override void ConsultaNumero(string numero)
+        {
+            Console.WriteLine($"Este é seu número de telefone: {numero} \n");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(11)98741-2510", modelo: "C21", imei: "581670-81", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.ConsultaNumero("(11)98741-2510");
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Pressione qualquer tecla para continuar o teste");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone(numero: "(21)98410-8799", modelo: "XR", imei: "410987-77", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.ConsultaNumero("(21)98410-8799");
+iphone.InstalarAplicativo("Apple Music");
+
+Console.WriteLine("Pressione qualquer tecla para finalizar o teste");
+Console.ReadLine();
+Console.Clear();
